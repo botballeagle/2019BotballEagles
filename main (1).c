66b90150e9410edabd2612a7062e62e8ee197690
botballeagle <48262255+botballeagle@users.noreply.github.com>
2019-03-06 14:33:44 -1000

    adding man.c file
    adding initial file

---
+++ main (1).c
@@ -0,0 +1,15 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    printf("Hello World\n");
+  create_connect(); 
+
+while ((get_create_lbump() == 0) && (get_create_rbump() == 0)) {
+create_drive_direct(100, 100); }
+    create_stop();
+  create_disconnect();
+  return 0;
+    
+  
+}
